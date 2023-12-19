41d0d866e593dc2792d3fabe4c21adad9413e044
Ярослав Молодцов <105973491+goodYaroslav@users.noreply.github.com>
2023-12-19 23:54:32 +0300

    🛵 refactor: service/post.go

--- internal/service/post.go
+++ internal/service/post.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"grates/internal/domain"
 	"grates/internal/repository"
 )
@@ -21,22 +22,24 @@ func NewPostService(postRepo repository.Post, commentRepo repository.Comment, li
 	}
 }
 
-func (p *PostService) Create(post domain.Post) (int, error) {
-	return p.postRepo.Create(post)
+// Create создает пост.
+func (s *PostService) Create(post domain.Post) (int, error) {
+	return s.postRepo.Create(post)
 }
 
-func (p *PostService) GetWithAdditions(postId int) (domain.Post, error) {
-	post, err := p.postRepo.Get(postId)
+// GetWithAdditions возвращает пост с добавленной информацией.
+func (s *PostService) GetWithAdditions(postId int) (domain.Post, error) {
+	post, err := s.postRepo.Get(postId)
 	if err != nil {
 		return post, err
 	}
 
-	post.Comments, err = p.commentRepo.GetPostComments(postId)
+	post.Comments, err = s.commentRepo.GetPostComments(postId)
 	if err != nil {
 		return post, err
 	}
 
-	post.LikesCount, err = p.likeRepo.GetPostLikesCount(postId)
+	post.LikesCount, err = s.likeRepo.GetPostLikesCount(postId)
 	if err != nil {
 		return post, err
 	}
@@ -44,22 +47,16 @@ func (p *PostService) GetWithAdditions(postId int) (domain.Post, error) {
 	return post, nil
 }
 
-func (p *PostService) GetUsersPosts(userId int) ([]domain.Post, error) {
-	posts, err := p.postRepo.UsersPosts(userId)
+// GetUsersPosts возвращает посты пользователя.
+func (s *PostService) GetUsersPosts(userId int) ([]domain.Post, error) {
+	posts, err := s.postRepo.UsersPosts(userId)
 	if err != nil {
 		return nil, err
 	}
 
-	for i, post := range posts {
-		posts[i].Comments, err = p.commentRepo.GetPostComments(post.Id)
-		if err != nil {
-			return nil, err
-		}
-
-		posts[i].LikesCount, err = p.likeRepo.GetPostLikesCount(post.Id)
-		if err != nil {
-			return nil, err
-		}
+	err = s.fillPostsWithAdditions(&posts)
+	if err != nil {
+		return nil, fmt.Errorf("fillPostsWithAdditions: %w", err)
 	}
 
 	return posts, nil
@@ -68,48 +65,55 @@ func (p *PostService) GetUsersPosts(userId int) ([]domain.Post, error) {
 // GetFriendsPosts возвращает посты друзей пользователя.
 // Метод сначала получает список id друзей пользователя, затем получает посты по этим id.
 // Далее проходится по этим постам и получает комментарии и количество лайков.
-func (p *PostService) GetFriendsPosts(userId int) ([]domain.Post, error) {
+func (s *PostService) GetFriendsPosts(userId int) ([]domain.Post, error) {
 	var posts []domain.Post
 
-	friendsIds, err := p.frienRepo.FriendUsersIds(userId)
+	friendsIds, err := s.frienRepo.FriendUsersIds(userId)
 	if err != nil {
 		return nil, err
 	}
 
-	posts, err = p.postRepo.PostsByUserIds(friendsIds)
+	posts, err = s.postRepo.PostsByUserIds(friendsIds)
 	if err != nil {
 		return nil, err
 	}
 
-	for i, post := range posts {
-		posts[i].Comments, err = p.commentRepo.GetPostComments(post.Id)
-		if err != nil {
-			return nil, err
-		}
-
-		posts[i].LikesCount, err = p.likeRepo.GetPostLikesCount(post.Id)
-		if err != nil {
-			return nil, err
-		}
+	err = s.fillPostsWithAdditions(&posts)
+	if err != nil {
+		return nil, fmt.Errorf("fillPostsWithAdditions: %w", err)
 	}
 
 	return posts, nil
 }
 
-func (p *PostService) Update(id int, newPost domain.PostUpdateInput) error {
-	return p.postRepo.Update(id, newPost)
+func (s *PostService) Update(id int, newPost domain.PostUpdateInput) error {
+	return s.postRepo.Update(id, newPost)
 }
 
-func (p *PostService) Delete(id int) error {
-	return p.postRepo.Delete(id)
+func (s *PostService) Delete(id int) error {
+	return s.postRepo.Delete(id)
 }
 
 // IsPostBelongsToUser проверяет принадлежит ли пользователю пост
-func (p *PostService) IsPostBelongsToUser(userId, postId int) (bool, error) {
-	post, err := p.postRepo.Get(postId)
+func (s *PostService) IsPostBelongsToUser(userId, postId int) (bool, error) {
+	post, err := s.postRepo.Get(postId)
 	if err != nil {
 		return false, err
 	}
 
 	return post.UsersId == userId, nil
+}
+
+// fillPostsWithAdditions заполняет посты дополнительной информацией.
+func (s *PostService) fillPostsWithAdditions(posts *[]domain.Post) error {
+	for i, post := range *posts {
+		p, err := s.GetWithAdditions(post.Id)
+		if err != nil {
+			return err
+		}
+
+		(*posts)[i] = p
+	}
+
+	return nil
 }
